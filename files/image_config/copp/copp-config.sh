a3d8bfea313d2a1cca234552cc5c90ac850163dd
Vineet Mittal <vineetmittal@microsoft.com>
2023-10-25 00:02:26 +0000

    [Chassis] : Update PG/Q from 4=>7 for CPU bound packets

--- files/image_config/copp/copp-config.sh
+++ files/image_config/copp/copp-config.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sonic-cfggen -d -t /usr/share/sonic/templates/copp_cfg.j2 > /etc/sonic/copp_cfg.json
+switch_type=`sonic-db-cli CONFIG_DB  hget 'DEVICE_METADATA|localhost' 'switch_type'`
+if [[$switch_type != 'voq']]; then
+    sonic-cfggen -d -t /usr/share/sonic/templates/copp_cfg.j2 > /etc/sonic/copp_cfg.json
+else
+    sonic-cfggen -d -t /usr/share/sonic/templates/copp_cfg_chassis.j2 > /etc/sonic/copp_cfg.json
+fi
